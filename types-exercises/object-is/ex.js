ae3493cf9c9b060eaa9e640f3f246c42eacdfd14
Evan <schafer.evan.r@gmail.com>
2021-01-19 21:50:41 -0800

    Complete object.is exercise

---
+++ types-exercises/object-is/ex.js
@@ -0,0 +1,61 @@
+// TODO: define polyfill for `Object.is(..)`
+// FINISHED
+if (!Object.is || true) {
+  Object.is = function(param1, param2) {
+    let nan1 = param1 !== param1;
+    let nan2 = param2 !== param2;
+    let nZero1 = 1/param1 === -Infinity;
+    let nZero2 = 1/param2 === -Infinity;
+    if (nan1 || nan2) {
+      return nan1 && nan2;
+    } else if (nZero1 || nZero2) {
+      return nZero1 && nZero2;
+    } else if (param1 === param2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+// tests:
+console.log(Object.is(42,42) === true);
+console.log(Object.is("foo","foo") === true);
+console.log(Object.is(false,false) === true);
+console.log(Object.is(null,null) === true);
+console.log(Object.is(undefined,undefined) === true);
+console.log(Object.is(NaN,NaN) === true);
+console.log(Object.is(-0,-0) === true);
+console.log(Object.is(0,0) === true);
+
+console.log(Object.is(-0,0) === false);
+console.log(Object.is(0,-0) === false);
+console.log(Object.is(0,NaN) === false);
+console.log(Object.is(NaN,0) === false);
+console.log(Object.is(42,"42") === false);
+console.log(Object.is("42",42) === false);
+console.log(Object.is("foo","bar") === false);
+console.log(Object.is(false,true) === false);
+console.log(Object.is(null,undefined) === false);
+console.log(Object.is(undefined,null) === false);
+
+// tests:
+console.log(Object.is(42,42) === true);
+console.log(Object.is("foo","foo") === true);
+console.log(Object.is(false,false) === true);
+console.log(Object.is(null,null) === true);
+console.log(Object.is(undefined,undefined) === true);
+console.log(Object.is(NaN,NaN) === true);
+console.log(Object.is(-0,-0) === true);
+console.log(Object.is(0,0) === true);
+
+console.log(Object.is(-0,0) === false);
+console.log(Object.is(0,-0) === false);
+console.log(Object.is(0,NaN) === false);
+console.log(Object.is(NaN,0) === false);
+console.log(Object.is(42,"42") === false);
+console.log(Object.is("42",42) === false);
+console.log(Object.is("foo","bar") === false);
+console.log(Object.is(false,true) === false);
+console.log(Object.is(null,undefined) === false);
+console.log(Object.is(undefined,null) === false);
